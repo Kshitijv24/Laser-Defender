6a2ae1e5f2e89b7be57be1a5a292ffaddb89bb4b
Kshitijv24 <kshitijv24@gmail.com>
2023-01-01 20:21:04 +0530

    Added some code to stop the player from going out of the screen
    Using Mathf.Clamp and mainCamera.ViewportToWorldPoint

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -5,17 +5,44 @@ public class Player : MonoBehaviour
 {
     [SerializeField] int moveSpeed;
 
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+
     Vector2 rawInput;
+    Vector2 minBound;
+    Vector2 maxBound;
+
+    Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        InitializeBounds();
+    }
 
     private void Update()
     {
         PlayerMovement();
     }
 
+    private void InitializeBounds()
+    {
+        minBound = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBound = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
     private void PlayerMovement()
     {
-        Vector3 delta = rawInput;
-        transform.position += delta * moveSpeed * Time.deltaTime;
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPosition = new Vector2();
+
+        newPosition.x = Mathf.Clamp(transform.position.x + delta.x, minBound.x + paddingLeft, maxBound.x - paddingRight);
+
+        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, minBound.y + paddingBottom, maxBound.y - paddingTop);
+
+        transform.position = newPosition;
     }
 
     private void OnMove(InputValue value)
