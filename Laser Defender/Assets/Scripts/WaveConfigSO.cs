4e12c712541a778be8fc6dfe93d5972fdc630893
Kshitijv24 <kshitijv24@gmail.com>
2023-01-03 17:54:46 +0530

    Implementing Pathfinding Of Enemies

---
+++ Laser Defender/Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float moveSpeed;
+
+    public Transform GetStartingWaypoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+
+    public List<Transform> GetWaypoints()
+    {
+        List<Transform> waypoints = new List<Transform>();
+
+        foreach(Transform child in pathPrefab)
+        {
+            waypoints.Add(child);
+        }
+
+        return waypoints;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+}
