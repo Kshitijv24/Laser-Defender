5e3fae1d4cdacdfc2786b06b0bdf27ab13be38e8
Kshitijv24 <kshitijv24@gmail.com>
2023-01-17 10:09:01 +0530

    added singleton pattern for the audio manager in our game

--- Laser Defender/Assets/Scripts/AudioManager.cs
+++ Laser Defender/Assets/Scripts/AudioManager.cs
@@ -10,6 +10,27 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioClip damageClip;
     [SerializeField] [Range(0f, 1f)] float damageVolume = 1f;
 
+    static AudioManager instance;
+
+    private void Awake()
+    {
+        ManageSingleton();
+    }
+
+    private void ManageSingleton()
+    {   
+        if(instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     public void PlayShootingClip()
     {
         PlayClip(shootingClip, shootingVolume);
